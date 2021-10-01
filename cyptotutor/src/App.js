ea1cd5ba50f73bd7f65eea081d2d442cabc0a49f
adetavernier <adetavernier@unomaha.edu>
2021-10-01 15:20:42 -0500

    Basic Question Page

---
+++ cyptotutor/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Questionform from  './components/questionform';
+
+function App() {
+  return (
+    <div className="App">
+      <Questionform/>
+    </div>
+  );
+}
+
+export default App;
