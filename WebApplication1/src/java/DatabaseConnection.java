f3314155a3c270aba37f1dc9411cb3d5b3a89ed0
Mantom112 <tnelson758@gmail.com>
2021-10-14 22:16:14 -0500

    small corrections

--- WebApplication1/src/java/DatabaseConnection.java
+++ WebApplication1/src/java/DatabaseConnection.java
@@ -15,10 +15,10 @@ public class DatabaseConnection {
         // Database name to access
         String dbName = "ctutordb";
         String dbUsername = "cpack";
-        String dbPassword = "ctutoR123!";
+        String dbPassword = "ctutoR123!";// hard coded so we know how to format the connection to the url
   
         Class.forName(dbDriver);
-        Connection con = DriverManager.getConnection("jdbc:mysql://104.131.172.9/ctutordb","cpack","ctutoR123!");
+        Connection con = DriverManager.getConnection("jdbc:mysql://104.131.172.9/ctutordb","cpack","ctutoR123!");// can use the variables if we format the end string to be like the one used here
         return con;
     }
 }
