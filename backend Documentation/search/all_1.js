a8a712f2c7b66f1df1d86c6ba550510f691259bc
Mantom112 <tnelson758@gmail.com>
2021-11-07 21:49:11 -0600

    moved the html java documentation under a better directory

--- backend Documentation/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['backendapplication_0',['BackEndApplication',['../classcom_1_1crypto__tutor_1_1_back_end_application.html',1,'com::crypto_tutor']]]
-];
--- backend Documentation/search/classes_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['question_0',['Question',['../classcom_1_1crypto__tutor_1_1models_1_1_question.html',1,'com::crypto_tutor::models']]],
-  ['questioncontroller_1',['QuestionController',['../classcom_1_1crypto__tutor_1_1controllers_1_1_question_controller.html',1,'com::crypto_tutor::controllers']]],
-  ['questionrepository_2',['QuestionRepository',['../interfacecom_1_1crypto__tutor_1_1repositories_1_1_question_repository.html',1,'com::crypto_tutor::repositories']]],
-  ['questionservice_3',['QuestionService',['../interfacecom_1_1crypto__tutor_1_1services_1_1_question_service.html',1,'com::crypto_tutor::services']]],
-  ['questionserviceimpl_4',['QuestionServiceImpl',['../classcom_1_1crypto__tutor_1_1services_1_1_question_service_impl.html',1,'com::crypto_tutor::services']]]
-];
