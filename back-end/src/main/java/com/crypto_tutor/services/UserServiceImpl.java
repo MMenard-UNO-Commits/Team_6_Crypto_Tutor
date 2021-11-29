a0ade334229df810dca1d0aabf71a63c79687ab1
MMenard-UNO-Commits <61168010+MMenard-UNO-Commits@users.noreply.github.com>
2021-11-29 14:21:38 -0600

    Added getStudents and pulled array from back end

--- back-end/src/main/java/com/crypto_tutor/services/UserServiceImpl.java
+++ back-end/src/main/java/com/crypto_tutor/services/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.crypto_tutor.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 import com.crypto_tutor.models.User;
 import com.crypto_tutor.repositories.UserRepository;
 
@@ -15,5 +17,10 @@ public class UserServiceImpl implements UserService{
     @Override
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getAllStudents() {
+        return userRepository.findAll();
     }
 }
--- cypto-tutor/src/components/Dashboard.js
+++ cypto-tutor/src/components/Dashboard.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-export default function Dashboard() {
-  return(
-    <h2>Dashboard</h2>
-  );
+export default class Dashboard extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  async componentDidMount(){
+    const response = await fetch("http://104.131.172.9:8080/users-test/user/getStudents");
+    const test = await response.json();
+    let tmpArray = [];
+    for (var i = 0; i < test.length; i++) {
+      tmpArray.push(test[i])
+    }
+    this.setState({users: tmpArray});
+    console.log(this.state.users);
+  }
+
+  render() {
+    return(
+      <html>
+      <h2> Dashboard </h2>
+      </html>
+    )
+  };
 }
