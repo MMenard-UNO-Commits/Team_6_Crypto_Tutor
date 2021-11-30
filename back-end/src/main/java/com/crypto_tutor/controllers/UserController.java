3fae88aa507f34cb4e10df958f537084a3266968
MMenard-UNO-Commits <61168010+MMenard-UNO-Commits@users.noreply.github.com>
2021-11-29 20:22:40 -0600

    removed unused import

--- back-end/src/main/java/com/crypto_tutor/controllers/UserController.java
+++ back-end/src/main/java/com/crypto_tutor/controllers/UserController.java
@@ -8,7 +8,6 @@ import java.util.List;
 import com.crypto_tutor.models.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
