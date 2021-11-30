44ac584d4c7b22cffe368c74427bcd6c129b2d7a
Mantom112 <tnelson758@gmail.com>
2021-11-29 18:56:44 -0600

    more tests to the Answer unit tests and Question Unit tests

--- back-end/src/test/java/com/crypto_tutor/backend/UnitTestQuestion.java
+++ back-end/src/test/java/com/crypto_tutor/backend/UnitTestQuestion.java
@@ -111,13 +111,147 @@ public class UnitTestQuestion {
         Question question = new Question();
         text = question.getUsername();
         assertEquals(null, text);
+    }
 
+    /**
+     * testting with 2 quetsions
+     */
+    @Test
+    void test2setCodeFrag()
+    {
+        String text, text2;
+        Question question = new Question();
+        Question question2 = new Question();
+        question.setCodeFragment("System.out.println(\"help me\");");
+        question2.setCodeFragment("return;");
+        text = question.getCodeFragment();
+        text2 = question2.getCodeFragment();
+        assertEquals("System.out.println(\"help me\");", text);
+        assertEquals("return;", text2);
+    }
 
+    @Test
+    void test2setUsername()
+    {
+        String text, text2;
+        Question question = new Question();
+        Question question2 = new Question();
+        question.setUsername("tester");
+        question2.setUsername("tester2;");
+        text = question.getUsername();
+        text2 = question2.getUsername();
+        assertEquals("tester", text);
+        assertEquals("tester2;", text2);
+    }
 
+    @Test
+    void test2setStudentId()
+    {
+        int text, text2;
+        Question question = new Question();
+        Question question2 = new Question();
+        question.setStudentId(12345678);
+        question2.setStudentId(23456789);
+        text = question.getStudentId();
+        text2 = question2.getStudentId();
+        assertEquals(12345678, text);
+        assertEquals(23456789, text2);
+    }
 
+    @Test
+    void test2setQuestion()
+    {
+        String text, text2;
+        Question question = new Question();
+        Question question2 = new Question();
+        question.setQuestion("help me");
+        question2.setQuestion("ayudame");
+        text = question.getQuestion();
+        text2 = question2.getQuestion();
+        assertEquals("help me", text);
+        assertEquals("ayudame", text2);
+    }
 
-       
+    /**
+     * now testing when setting something that is already set
+     */
+    @Test
+    void settingCodeFragAgain()
+    {
+        String text, text2;
+        Question question = new Question();
+        Question question2 = new Question();
+        question.setCodeFragment("System.out.println(\"help me\");");
+        question2.setCodeFragment("return;");
+        text = question.getCodeFragment();
+        text2 = question2.getCodeFragment();
+        assertEquals("System.out.println(\"help me\");", text);
+        assertEquals("return;", text2);
+        question.setCodeFragment("something else");
+        question2.setCodeFragment("different again");
+        text = question.getCodeFragment();
+        text2 = question2.getCodeFragment();
+        assertEquals("something else", text);
+        assertEquals("different again", text2);
+    }
 
-        
+    @Test
+    void settingQuestionAgain()
+    {
+        String text, text2;
+        Question question = new Question();
+        Question question2 = new Question();
+        question.setQuestion("i am ripping my hair out help me");
+        question2.setQuestion("what is wrong with my code");
+        text = question.getQuestion();
+        text2 = question2.getQuestion();
+        assertEquals("i am ripping my hair out help me", text);
+        assertEquals("what is wrong with my code", text2);
+        question.setQuestion("i would please like to know if anyone has any different ideas for what is wrong with my code");
+        question2.setQuestion("i would like to know if there is a character limit to this system and if so what is it and will i break the system by typing something that is entire too long for the system to handle. Will i be albe to keep going forver and forever. please send help my way if you can read this message.");
+        text = question.getQuestion();
+        text2 = question2.getQuestion();
+        assertEquals("i would please like to know if anyone has any different ideas for what is wrong with my code", text);
+        assertEquals("i would like to know if there is a character limit to this system and if so what is it and will i break the system by typing something that is entire too long for the system to handle. Will i be albe to keep going forver and forever. please send help my way if you can read this message.", text2);
+    }
+
+    @Test
+    void settingUsernameAgain()
+    {
+        String text, text2;
+        Question question = new Question();
+        Question question2 = new Question();
+        question.setUsername("wsmith");
+        question2.setUsername("ggadot");
+        text = question.getUsername();
+        text2 = question2.getUsername();
+        assertEquals("wsmith", text);
+        assertEquals("ggadot", text2);
+        question.setUsername("wbuffet");
+        question2.setUsername("ahamilton");
+        text = question.getUsername();
+        text2 = question2.getUsername();
+        assertEquals("wbuffet", text);
+        assertEquals("ahamilton", text2);
+    }
+
+    @Test
+    void settingSchoolId()
+    {
+        int text, text2;
+        Question question = new Question();
+        Question question2 = new Question();
+        question.setStudentId(86752431);
+        question2.setStudentId(10101011);
+        text = question.getStudentId();
+        text2 = question2.getStudentId();
+        assertEquals(86752431, text);
+        assertEquals(10101011, text2);
+        question.setStudentId(94326754);
+        question2.setStudentId(10304050);
+        text = question.getStudentId();
+        text2 = question2.getStudentId();
+        assertEquals(94326754, text);
+        assertEquals(10304050, text2);
     }
 }
