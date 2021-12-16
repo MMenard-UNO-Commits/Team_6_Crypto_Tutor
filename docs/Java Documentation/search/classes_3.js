0dfca09c571f5cdc03bd294f4296acdb59192aa1
Mantom112 <tnelson758@gmail.com>
2021-12-15 21:07:50 -0600

    getting rid of excess files

--- docs/Java Documentation/search/classes_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['servletinitializer_0',['ServletInitializer',['../classcom_1_1crypto__tutor_1_1_servlet_initializer.html',1,'com::crypto_tutor']]]
-];
--- docs/Java Documentation/search/functions_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['add_0',['add',['../classcom_1_1crypto__tutor_1_1controllers_1_1_answer_controller.html#aa8780109276236cea1852a0dbb0d5145',1,'com.crypto_tutor.controllers.AnswerController.add()'],['../classcom_1_1crypto__tutor_1_1controllers_1_1_question_controller.html#ad2bf4e21d13e8baa8e5a00bfe7fb36b0',1,'com.crypto_tutor.controllers.QuestionController.add()']]],
-  ['answer_1',['Answer',['../classcom_1_1crypto__tutor_1_1models_1_1_answer.html#a070636380930a9893a961132c4160ccb',1,'com::crypto_tutor::models::Answer']]]
-];
